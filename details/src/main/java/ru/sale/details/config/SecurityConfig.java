9584a5669b961fc23b079ba4f6fd1d0004aadf15
Даниил <daneil50005@yandex.ru>
2024-04-04 02:11:30 +0300

    fix bug

--- details/src/main/java/ru/sale/details/config/SecurityConfig.java
+++ details/src/main/java/ru/sale/details/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize -> authorize
+        http.cors().and().csrf().disable().authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/user/**").hasRole("USER")
                         .anyRequest().permitAll())
